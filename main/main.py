1adf29997c6faa75f485bada1e205123679db6e7
aasprt <123574407+aasprt@users.noreply.github.com>
2023-02-02 20:51:11 +0100

    PID line follower e modello 3d

--- main/main.py
+++ main/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -7,14 +6,35 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
 ev3 = EV3Brick()
+motor_left = Motor(Port.A)
+motor_right = Motor(Port.D)
+light_left = Motor(Port.S1)
+light_right = Motor(Port.S2)
 
+robot = DriveBase(motor_left, motor_right, wheel_diameter=55.5, axle_track=104)
 
-# Write your program here.
+taget_path = 45
+proportional_gain = 0.5
+dirve_speed = 50
+go = true
+
 ev3.speaker.beep()
+while go:
+  #obstacle_avoidance
+  
+
+  #line_follower
+  if light_right.reflection() < target_path or light_left > target_path:
+    motor_left.run(int(proportional_gain * light_left.reflection() - target_path))
+    motor_right.run(drive_speed)
+  elif light_left.reflection() < target_path or light_right > target_path:
+    motor_right.run(int(proportional_gain * light_left.reflection() - target_path))
+    motor_left.run(drive_speed)
+  else:
+    motor_left.run(drive_speed)
+    motor_right.run(drive_speed)
+  
+  if Button.DOWN in brick.buttons():
+    go = false
+    
